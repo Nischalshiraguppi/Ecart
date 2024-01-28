fbed256415dc9e525f4392e3cd081266c01a808f
Nischal <nischalshiraguppi1@gmail.com>
2024-01-28 23:40:14 +0530

    added CRED operations

--- EcartProj/Controllers/CategoryController.cs
+++ EcartProj/Controllers/CategoryController.cs
@@ -1,12 +1,96 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcartProj.Data;
+using EcartProj.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcartProj.Controllers
 {
-    public class CategoryController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			List<Category> db = _db.categories.ToList();
+			return View(db);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category category)
+		{
+			if (category.Name == category.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "Display Order can't be same as Name.");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.categories.Add(category);
+				_db.SaveChanges();
+				TempData["success"] = "Category Created";
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+
+			if (ModelState.IsValid)
+			{
+				_db.categories.Update(category);
+				_db.SaveChanges();
+				TempData["success"] = "Category Edited";
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category? category = _db.categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			_db.categories.Remove(category);
+			_db.SaveChanges();
+			TempData["success"] = "Category Deleted";
+			return RedirectToAction("Index");
+
+		}
+	}
 }
--- EcartProj/Models/Category.cs
+++ EcartProj/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcartProj.Models
 {
@@ -7,7 +8,11 @@ namespace EcartProj.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Calegory Name")]
         public string? Name { get; set; }
+        [DisplayName("Display Name")]
+        [Range(0, 100)]
         public int DisplayOrder { get; set; }
 
     }
